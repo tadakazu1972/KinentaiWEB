da5bdaee182ed6f9e62b853315441c5d5bf3c1c2
tadakazu1972 <tadakazu1972@gmail.com>
2017-09-05 17:38:56 +0900

    initial commit

---
+++ home.js
@@ -0,0 +1,55 @@
+//Home画面描画
+function clickButton(num){
+  //window.confirm(num+"がクリックされたよ");
+  var num = encodeURIComponent(num);
+  var param = "num="+num;
+  location.href="./map.html?"+param;
+}
+
+function clickButton1(){
+  location.href = "./map.html?num=0";
+}
+
+function clickButton2(){
+  location.href = "./map.html?num=1";
+}
+
+function clickButton3(){
+  location.href = "./map.html?num=2";
+}
+
+function clickButton4(){
+  location.href = "./map.html?num=3";
+}
+
+function clickButton5(){
+  location.href = "./map.html?num=4";
+}
+
+function clickButton6(){
+  location.href = "./map.html?num=5";
+}
+
+function clickButton7(){
+  location.href = "./map.html?num=6";
+}
+
+function clickButton8(){
+  location.href = "./map.html?num=7";
+}
+
+function clickButton9(){
+  location.href = "./map.html?num=8";
+}
+
+function clickButton10(){
+  location.href = "./map.html?num=9";
+}
+
+function clickButton11(){
+  location.href = "./map.html?num=10";
+}
+
+function clickButton12(){
+  location.href = "./map.html?num=11";
+}
