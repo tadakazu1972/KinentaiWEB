bc6419b07aaec922f30663e0ddde21730d54c23e
tadakazu1972 <tadakazu1972@gmail.com>
2017-09-08 17:54:20 +0900

    fix showResult

--- home.js
+++ home.js
@@ -4,9 +4,9 @@ function clickButton1(){
               + '<div id="dialog_back"></div>'
               + '<div id="dialog_body">'
               + '最大震度は？<br>'
-              + '<button class="list" type="button" onclick="selectArea7()">◾️震度７(特別区６強)</button><br>'
-              + '<button class="list" type="button" onclick="showResult()">◾️震度６強(特別区６弱)</button><br>'
-              + '<button class="list" type="button" onclick="showResult()">◾️震度６弱(特別区５強、政令市５強)</button><br>'
+              + '<button class="list" type="button" onclick="selectArea(1)">◾️震度７(特別区６強)</button><br>'
+              + '<button class="list" type="button" onclick="selectArea(2)">◾️震度６強(特別区６弱)</button><br>'
+              + '<button class="list" type="button" onclick="selectArea(3)">◾️震度６弱(特別区５強、政令市５強)</button><br>'
               + '<br>'
               + '<button class="cancel" type="button" onclick="closeDialog()">閉じる</button>'
               + '</div>';
@@ -72,13 +72,20 @@ function closeDialog(){
   delNode.parentNode.removeChild(delNode);
 }
 
-function selectArea7(){
-  var title = "■最大震度７(特別区６強)<br>　震央管轄都道府県は?<br>";
+function selectArea(num){
+  var title = "";
+  if (num==1) {
+    title = "■最大震度７(特別区６強)<br>　震央管轄都道府県は?<br>";
+  } else if (num==2) {
+    title = "■最大震度６強(特別区６弱)<br>　震央管轄都道府県は?<br>";
+  } else if (num==3) {
+    title = "■最大震度６弱(特別区５強、政令市５強)<br>　震央管轄都道府県は?<br>";
+  }
   var prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"];
   var base = document.getElementById("dialog2_prefectures");
   var item = "";
   for(var i=0;i<prefectures.length;i++){
-    item = item + '<button class="prefecture" type="button" onclick="showResult(' + i + ')">' + prefectures[i] + '</button>';
+    item = item + '<button class="prefecture" type="button" onclick="showResult(' + i + ',' + num + ')">' + prefectures[i] + '</button>';
   }
   //ダイアログコンテンツ統合
   var html = document.getElementById("dialog_content").innerHTML;
@@ -93,8 +100,16 @@ function selectArea7(){
   document.getElementById("dialog_content").innerHTML = html;
 }
 
-function showResult(num){
-  var s = "";
+function showResult(prefIndex, num){
+  var title = "";
+  if (num==1) {
+    title = "■最大震度７(特別区６強)<br>";
+  } else if (num==2) {
+    title = "■最大震度６強(特別区６弱)<br>";
+  } else if (num==3) {
+    title = "■最大震度６弱(特別区５強、政令市５強)<br>";
+  }
+  var result = "";
   var csvArray = new Array();
   //csvファイル読み込み
   var xhr = new XMLHttpRequest();
@@ -102,13 +117,36 @@ function showResult(num){
     var tempArray = xhr.responseText.split("\n");
     for(var i=1; i<tempArray.length; i++){ //i=1はヘッダーを読み込ませないため
       csvArray[i] = tempArray[i].split(",");
-      var data = csvArray[i];
-      if ((num+1)==i){
-        s = data[0] + '<br>' + data[1] + '<br>' + data[2] + '<br>' + data[3];
-      }
     }
-    window.confirm(csvArray[num+1]);
+    var data = csvArray[prefIndex+1];
+    var result =   '　' + data[0] + '<br>'
+                 + '<br>'
+                 + '・指揮支援隊<br>'
+                 + '　' + data[1] + '<br>'
+                 + '<br>'
+                 + '・大阪府大隊(陸上)<br>'
+                 + '　' + data[2] + '<br>'
+                 + '<br>'
+                 + '・大阪府大隊(航空)<br>'
+                 + '　' + data[3];
+    var html =     '<div id="dialog">'
+                 + '<div id="dialog3_back"></div>'
+                 + '<div id="dialog3_body">'
+                 + title
+                 + result
+                 + '<br>'
+                 + '<br>'
+                 + '<button class="cancel" type="button" onclick="closeDialog()">閉じる</button>'
+                 + '</div>';
+    document.getElementById("dialog_content").innerHTML = html;
   };
-  xhr.open("get", "assets/riku7.csv");
+  if (num==1) {
+    xhr.open("get", "assets/riku7.csv");
+  } else if (num==2) {
+    xhr.open("get", "assets/riku6strong.csv");
+  } else if (num==3) {
+    xhr.open("get", "assets/riku6weak.csv");
+  }
+
   xhr.send(null);
 }
